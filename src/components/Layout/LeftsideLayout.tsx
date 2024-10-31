6409184829096be398ba6538d39f734d3b89168f
njaina <hei.njaina.2@gmail.com>
2024-10-31 21:03:10 +0300

    chore: update

--- src/components/Layout/LeftsideLayout.tsx
+++ src/components/Layout/LeftsideLayout.tsx
@@ -16,9 +16,13 @@ export const LeftsideLayout: React.FC<LeftsideLayoutProps> = ({
 }) => {
   return (
     <div>
-      <ul className="flex flex-col items-center space-y-6 bg-gray-800 rounded-lg py-8 px-6 shadow-lg h-screen fixed top-0 left-0 w-1/6 "> 
+      <ul className="flex flex-col items-center text-center space-y-6 bg-gray-800 rounded-lg py-8 px-6 shadow-lg h-screen fixed top-0 left-0 w-1/6 ">
         {options.map((option) => (
-          <li  key={option.value} onClick={() => onMenuSelect(option.value)} className="text-white w-full text-center py-3 px-5 rounded-md hover:bg-teal-600 hover:text-gray-100 transition-all duration-300 cursor-pointer">
+          <li
+            key={option.value}
+            onClick={() => onMenuSelect(option.value)}
+            className="text-white w-full text-center py-3 px-5 rounded-md hover:bg-teal-600 hover:text-gray-100 transition-all duration-300 cursor-pointer"
+          >
             {option.label}
           </li>
         ))}
